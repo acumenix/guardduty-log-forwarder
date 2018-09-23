99b1f31d51d3833e950e1b638fa1fdba1e33c47f
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2018-09-23 22:32:40 +0900

    add a deploy script for CFn to all region

--- scripts/deploy-to-all-region.sh
+++ scripts/deploy-to-all-region.sh
@@ -2,11 +2,25 @@
 
 REGIONS=(`cat scripts/guardduty-regions.txt | tr '\n' ' '`)
 
+if [ $# -le 3 ]; then
+    echo "usage) $0 StackName DstS3Region DstS3Bucket DstS3Prefix [LambdaArn]"
+    exit 1
+fi
+
+if [ "$5" != "" ]; then
+   LAMBDA_ARN="LambdaArn=$5"
+else
+   LAMBDA_ARN=""
+fi
+
+echo $LAMBDA_ARN
+
 for region in ${REGIONS[@]}; do
-    aws --region $region cloudformation package \
-                --template-file template.yml \
-                --s3-bucket cfn-assets.$region \
-                --s3-prefix guardduty-log-forwarder/functions \
-                --output-template-file sam.$region.yml
-    aws s3 cp sam.$region.yml s3://cfn-assets.$region/guardduty-log-forwarder/templates/latest.yml
+    TEMPLATE="sam.$region.yml"
+    curl -sf -o $TEMPLATE https://s3-$region.amazonaws.com/cfn-assets.$region/guardduty-log-forwarder/templates/latest.yml
+    aws --region $region cloudformation deploy \
+        --template-file $TEMPLATE \
+        --stack-name $1 \
+        --capabilities CAPABILITY_IAM \
+        --parameter-overrides DstS3Region=$2 DstS3Bucket=$3 DstS3Prefix=$4 $LAMBDA_ARN
 done
